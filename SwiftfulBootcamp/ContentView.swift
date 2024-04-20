072a672a348afc0bb45afaf35036a57c397ce2ae
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-04-20 15:39:17 +0500

    Initial Commit

---
+++ SwiftfulBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulBootcamp/SwiftfulBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulBootcampApp.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
