abde0e2225b19e30b52cd78bb26d0775c9735c88
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-06-18 20:06:18 +0500

    Dynamict text update

---
+++ SwiftfulBootcamp/AccessibilityTextBootcamp.swift
@@ -0,0 +1,51 @@
+//
+//  AccessibilityTextBootcamp.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 6/18/24.
+//
+
+import SwiftUI
+
+struct AccessibilityTextBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+extension ContentSizeCategory {
+    var cusomMinScaleFactor: CGFloat {
+        switch self {
+        case .extraSmall:
+            return 1.0
+        case .small:
+            return 1.0
+        case .medium:
+            return 1.0
+        case .large:
+            return 1.0
+        case .extraLarge:
+            return 1.0
+        case .extraExtraLarge:
+            return 1.0
+        case .extraExtraExtraLarge:
+            return 1.0
+        case .accessibilityMedium:
+            return 1.0
+        case .accessibilityLarge:
+            return 1.0
+        case .accessibilityExtraLarge:
+            return 1.0
+        case .accessibilityExtraExtraLarge:
+            return 1.0
+        case .accessibilityExtraExtraExtraLarge:
+            return 1.0
+        @unknown default:
+            fatalError()
+        }
+    }
+}
+
+#Preview {
+    AccessibilityTextBootcamp()
+}
