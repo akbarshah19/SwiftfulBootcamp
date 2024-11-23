bd0d933c6cf4325446d98358ca7e15ebdc7df037
akbarshah19 <akbarshah19@icloud.com>
2024-11-23 17:33:29 +0500

    PreferenceKey bootcamp update

---
+++ SwiftfulBootcamp/Advanced/ScrollViewOffsetPreferenceKeyBootcamp.swift
@@ -0,0 +1,56 @@
+//
+//  ScrollViewOffsetPreferenceKeyBootcamp.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 11/23/24.
+//
+
+import SwiftUI
+
+struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+    
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+struct ScrollViewOffsetPreferenceKeyBootcamp: View {
+    
+    let title: String = "Title"
+    @State private var scrollViewOffset: CGFloat = .zero
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text(title)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .opacity(Double(scrollViewOffset)/78.0)
+                    .background {
+                        GeometryReader { geo in
+                            Text("")
+                                .preference(key: ScrollViewOffsetPreferenceKey.self, value: geo.frame(in: .global).minY)
+                        }
+                    }
+                
+                ForEach(0..<50) { _ in
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.green)
+                        .frame(height: 200)
+                }
+            }
+            .padding()
+        }
+        .overlay(Text("\(scrollViewOffset)"))
+        .onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
+            scrollViewOffset = value
+        }
+    }
+}
+
+#Preview {
+    ScrollViewOffsetPreferenceKeyBootcamp()
+}
