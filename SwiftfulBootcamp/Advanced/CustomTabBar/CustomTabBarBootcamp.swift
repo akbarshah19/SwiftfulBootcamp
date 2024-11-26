a2e9e90a9783bbd936f467449d5a5b44348fba95
akbarshah19 <akbarshah19@icloud.com>
2024-11-26 23:34:33 +0500

    CustomTabBarBootcamp update

---
+++ SwiftfulBootcamp/Advanced/CustomTabBar/CustomTabBarBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBarBootcamp.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 11/26/24.
+//
+
+import SwiftUI
+
+struct CustomTabBarBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBarBootcamp()
+}
