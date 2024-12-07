663225f9e4da331593ef1b46c2bc44b37604e1d5
akbarshah19 <akbarshah19@icloud.com>
2024-12-07 23:55:46 +0500

    CustomTabBar update

--- SwiftfulBootcamp/Advanced/CustomTabBar/CustomTabBarBootcamp.swift
+++ SwiftfulBootcamp/Advanced/CustomTabBar/CustomTabBarBootcamp.swift
@@ -7,18 +7,85 @@
 
 import SwiftUI
 
-struct CustomTabBarBootcamp: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
 struct TabBarItem: Hashable {
     let iconName: String
     let title: String
     let color: Color
 }
 
+struct CustomTabBarBootcamp: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomTabBarView: View {
+    
+    let tabs: [TabBarItem]
+    @Binding var selection: TabBarItem
+    
+    var body: some View {
+        HStack {
+            ForEach(tabs, id: \.self) { tab in
+                tabView(tab: tab)
+                    .onTapGesture {
+                        switchTab(tab: tab)
+                    }
+            }
+        }
+    }
+    
+    private func tabView(tab: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tab.iconName)
+                .font(.headline)
+            
+            Text(tab.title)
+                .font(.system(size: 10, weight: .semibold))
+        }
+        .foregroundStyle(tab.color)
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity)
+        .background(tab.color.opacity(0.2))
+        .clipShape(.rect(cornerRadius: 20))
+    }
+    
+    private func switchTab(tab: TabBarItem) {
+        withAnimation(.easeInOut) {
+            selection = tab
+        }
+    }
+}
+
+struct CustomTabBarContainerView<Content: View>: View {
+    
+    @Binding var selection: TabBarItem
+    let content: Content
+    @State private var tabs: [TabBarItem] = []
+    
+    init(selection: Binding<TabBarItem>, @ViewBuilder content: () -> Content) {
+        self._selection = selection
+        self.content = content()
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ZStack {
+                content
+            }
+            
+            CustomTabBarView(tabs: tabs, selection: $selection)
+        }
+    }
+}
+
 #Preview {
-    CustomTabBarBootcamp()
+    static let tabs: [TabBarItem] = [
+        .init(iconName: "house", title: "Home", color: .red),
+        .init(iconName: "heart", title: "Favourites", color: .blue),
+        .init(iconName: "person", title: "Profile", color: .green)
+    ]
+    
+    CustomTabBarContainerView(selection: .constant(.init(iconName: "house", title: "Home")) {}
 }
