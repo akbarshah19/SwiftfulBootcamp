e11f2c0882ef02012eb50675304ff9bd7d3ed643
akbarshah19 <akbarshah19@icloud.com>
2024-12-11 23:44:15 +0500

    CustomNavBarBootcamp update

---
+++ SwiftfulBootcamp/Advanced/CustomNavBarBootcamp.swift
@@ -0,0 +1,26 @@
+//
+//  CustomNavBarBootcamp.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 12/11/24.
+//
+
+import SwiftUI
+
+struct CustomNavBarBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNavBarView()
+}
+
+struct CustomNavBarView: View {
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
