c53035e431d33f9a03be6cbe88f72c58af4d0ba5
akbarshah19 <akbarshah19@icloud.com>
2024-12-12 23:50:31 +0500

    ProtocolBootcamo init

---
+++ SwiftfulBootcamp/Advanced/ProtocolBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ProtocolBootcamo.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 12/12/24.
+//
+
+import SwiftUI
+
+struct ProtocolBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProtocolBootcamp()
+}
