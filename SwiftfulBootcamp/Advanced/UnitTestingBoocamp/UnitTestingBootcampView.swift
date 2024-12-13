d593043218017aa95890cf76ab605ef0c6973528
akbarshah19 <akbarshah19@icloud.com>
2024-12-13 23:53:27 +0500

    UnitTestingBootcamp update

---
+++ SwiftfulBootcamp/Advanced/UnitTestingBoocamp/UnitTestingBootcampView.swift
@@ -0,0 +1,22 @@
+//
+//  UnitTestingBootcamp.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 12/13/24.
+//
+
+import SwiftUI
+
+class UnitTestingBootcampViewModel: ObservableObject {
+    
+}
+
+struct UnitTestingBootcampView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnitTestingBootcampView()
+}
