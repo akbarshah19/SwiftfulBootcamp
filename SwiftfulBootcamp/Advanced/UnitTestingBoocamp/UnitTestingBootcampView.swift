275ffa189c2b6f98aa5e08c9f9b5df69c5f60c49
akbarshah19 <akbarshah19@icloud.com>
2024-12-14 20:35:17 +0500

    Unit Testing escaping and combine functions

--- SwiftfulBootcamp/Advanced/UnitTestingBoocamp/UnitTestingBootcampView.swift
+++ SwiftfulBootcamp/Advanced/UnitTestingBoocamp/UnitTestingBootcampView.swift
@@ -6,17 +6,70 @@
 //
 
 import SwiftUI
+import Combine
+
+protocol DataServiceProtocol {
+    func downloadItemsWithEscaping(completion: @escaping (_ items: [String]) -> Void)
+    func downloadItemsWithCombine() -> AnyPublisher<[String], Error>
+}
+
+class MockDataService: DataServiceProtocol {
+    
+    let items: [String]
+    
+    init(items: [String]?) {
+        self.items = items ?? ["One", "Two", "Three"]
+    }
+    
+    func downloadItemsWithEscaping(completion: @escaping (_ items: [String]) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            completion(self.items)
+        }
+    }
+    
+    func downloadItemsWithCombine() -> AnyPublisher<[String], Error> {
+        Just(items)
+            .tryMap { publishedItems in
+                guard !publishedItems.isEmpty else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                return publishedItems
+            }
+            .eraseToAnyPublisher()
+    }
+}
 
 class UnitTestingBootcampViewModel: ObservableObject {
-    @Published var isPremium: Bool
-    @Published var dataArrray: [String] = []
     
-    init(isPremium: Bool) {
+    @Published var isPremium: Bool
+    @Published var dataArray: [String] = []
+    let dataService: DataServiceProtocol
+    var cancellables = Set<AnyCancellable>()
+    
+    init(isPremium: Bool, dataService: DataServiceProtocol = MockDataService(items: nil)) {
         self.isPremium = isPremium
+        self.dataService = dataService
     }
     
     func addItem(item: String) {
-        dataArrray.append(item)
+        dataArray.append(item)
+    }
+    
+    func downloadWithEscaping() {
+        dataService.downloadItemsWithEscaping {  [weak self] items in
+            self?.dataArray = items
+        }
+    }
+    
+    func downloadWithCombine() {
+        dataService.downloadItemsWithCombine()
+            .sink { _ in
+                
+            } receiveValue: { [weak self] items in
+                self?.dataArray = items
+            }
+            .store(in: &cancellables)
     }
 }
 
--- SwiftfulBootcamp_Tests/UnitTestingBootcampViewModel_Test.swift
+++ SwiftfulBootcamp_Tests/UnitTestingBootcampViewModel_Test.swift
@@ -6,16 +6,20 @@
 //
 
 import XCTest
+import Combine
 @testable import SwiftfulBootcamp
 
 final class UnitTestingBootcampViewModel_Test: XCTestCase {
+    
+    var vm: UnitTestingBootcampViewModel?
+    var cancellables = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        vm = nil
     }
 
     func test_UnitTestingBootcampViewModel_isPremium_shouldBeInjectedValue_stress() {
@@ -28,12 +32,60 @@ final class UnitTestingBootcampViewModel_Test: XCTestCase {
     
     func test_UnitTestingBootcampViewModel_dataArray_shouldbeEmpty() {
         let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
-        XCTAssertTrue(vm.dataArrray.isEmpty)
+        XCTAssertTrue(vm.dataArray.isEmpty)
     }
     
     func test_UnitTestingBootcampViewModel_dataArray_shoulAddItems() {
-        let vm = UnitTestingBootcampViewModel(isPremium: Bool.random())
+        guard let vm = vm else {
+            XCTFail()
+            return
+        }
+        
         vm.addItem(item: "Hello")
-        XCTAssertTrue(!vm.dataArrray.isEmpty)
+        XCTAssertTrue(!vm.dataArray.isEmpty)
+    }
+    
+    func test_UnitTestingBootcampViewModel_downloadWithEscping_shouldReturnItems() {
+        guard let vm = vm else {
+            XCTFail()
+            return
+        }
+        
+        let expectation = XCTestExpectation(description: "Should return items after 2 secs")
+        
+        vm.$dataArray
+            .dropFirst()
+            .sink { items in
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        vm.downloadWithEscaping() //this func is async
+        
+        wait(for: [expectation], timeout: 5)
+        
+        XCTAssertGreaterThan(vm.dataArray.count, 0)
+    }
+    
+    func test_UnitTestingBootcampViewModel_downloadWithCombine_shouldReturnItems() {
+        guard let vm = vm else {
+            XCTFail()
+            return
+        }
+                
+        let expectation = XCTestExpectation(description: "Should return items after 2 secs")
+        
+        vm.$dataArray
+            .dropFirst()
+            .sink { items in
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        vm.downloadWithCombine() //this func is async
+        
+        wait(for: [expectation], timeout: 5)
+        
+        XCTAssertGreaterThan(vm.dataArray.count, 0)
     }
 }
