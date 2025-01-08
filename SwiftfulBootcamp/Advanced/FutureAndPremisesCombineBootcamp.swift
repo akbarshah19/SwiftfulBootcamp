5849dfa1b5190d3e5129315da968a1a8a98df1c5
akbarshah19 <akbarshah19@icloud.com>
2025-01-08 23:51:38 +0500

    Initial lesson commit

---
+++ SwiftfulBootcamp/Advanced/FutureAndPremisesCombineBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FutureAndPremisesCombineBootcamp.swift
+//  SwiftfulBootcamp
+//
+//  Created by Akbarshah Jumanazarov on 1/8/25.
+//
+
+import SwiftUI
+
+struct FutureAndPremisesCombineBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FutureAndPremisesCombineBootcamp()
+}
